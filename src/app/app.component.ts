82a4b27a03b7d305553f7c32aab32119b3ebe73c
Mayank Gupta <mayankgupta8995@gmail.com>
2021-04-15 12:18:30 +0530

    navigator test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'MultiAppProject';
+
   constructor(){
+
+  }
+  ngOnInit(): void {
+    this.showCordinate();
   }
   onActivate(event){
     console.log("event>>",event);
   }
+
+  private showCordinate(){
+    // if (navigator.geolocation) {
+    //   navigator.geolocation.getCurrentPosition(this.showPosition);
+    // }
+  }
+   showPosition(position) {
+   console.log(position);
+  }
+  
 }
